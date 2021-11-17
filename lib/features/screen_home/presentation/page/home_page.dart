2648b9184f7e8f39c2faac44874e500b31ba3dbc
yevgeniy.dadonov <yevgeniy.dadonov@gmail.com>
2021-11-17 14:19:07 +0300

    - PageViewer для Home;

--- lib/features/screen_home/presentation/page/home_page.dart
+++ lib/features/screen_home/presentation/page/home_page.dart
@@ -16,58 +16,129 @@ import 'package:sport_stats_live/features/screen_settings/presentation/page.dart
 import 'package:sport_stats_live/features/screen_teams_list/presentation/page/team_list_page.dart';
 import 'package:sport_stats_live/features/team/domain/bloc/bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<Widget> pages;
+  int _selectedIndex = 0;
+
+  final PageController pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
+
+  @override
+  void initState() {
+    pages = <Widget>[
+      MatchListPage.view(context.read<MatchBloc>()),
+      TeamsListPage.view(BlocProvider.of<TeamsBloc>(context)),
+      ConfigurationScreen.view(
+        configurationBloc: BlocProvider.of<ConfigurationBloc>(context),
+        parameterBloc: BlocProvider.of<ParameterBloc>(context),
+      ),
+      const SettingsScreen(),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TabBloc, AppTab>(
       builder: (context, activeTab) {
-        final textColor = ThemeHolder.of(context).main;
         return Scaffold(
-          //TODO: Вынести AppBar в отдельный Bloc
-          appBar: AppBar(
-            backgroundColor: ThemeHolder.of(context).card,
-            elevation: 2,
-            shadowColor: ThemeHolder.of(context).cardShadow,
-            title: Text(
-              "Заголовок",
-              style: ThemeHolder.of(context).textStyle.h2(color: textColor),
-            ),
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 16),
-              child: AppIcon(
-                icon: AppIcons.logo,
-                color: ThemeHolder.of(context).warning,
-                height: 50,
-              ),
-            ),
-            actions: [],
+          appBar: _buildAppBar(context, activeTab),
+          body: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              BlocProvider.of<TabBloc>(context).add(TabUpdated(AppTab.values[index]));
+            },
+            children: pages,
           ),
-          body: _getBody(context, activeTab),
           bottomNavigationBar: TabSelector(
             activeTab: activeTab,
-            onTabSelected: (tab) =>
-                BlocProvider.of<TabBloc>(context).add(TabUpdated(tab)),
+            onTabSelected: (tab) {
+              BlocProvider.of<TabBloc>(context).add(TabUpdated(tab));
+              pageController.jumpToPage(tab.index);
+            }
           ),
         );
       },
     );
   }
 
-  Widget _getBody(BuildContext context, AppTab appTab) {
+  AppBar _buildAppBar(BuildContext context, AppTab appTab) {
+    final textColor = ThemeHolder.of(context).main;
+
     switch (appTab) {
       case AppTab.matches:
-        return MatchListPage.view(context.read<MatchBloc>());
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: ThemeHolder.of(context).card,
+          elevation: 2,
+          shadowColor: ThemeHolder.of(context).cardShadow,
+          title: Text(
+            "Матчи",
+            style: ThemeHolder.of(context).textStyle.h2(color: textColor),
+          ),
+          leading: const _AppLogo(),
+        );
       case AppTab.teams:
-        return TeamsListPage.view(BlocProvider.of<TeamsBloc>(context));
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: ThemeHolder.of(context).card,
+          elevation: 2,
+          shadowColor: ThemeHolder.of(context).cardShadow,
+          title: Text(
+            "Команды",
+            style: ThemeHolder.of(context).textStyle.h2(color: textColor),
+          ),
+          leading: const _AppLogo(),
+        );
       case AppTab.configuration:
-        return ConfigurationScreen.view(
-          configurationBloc: BlocProvider.of<ConfigurationBloc>(context),
-          parameterBloc: BlocProvider.of<ParameterBloc>(context),
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: ThemeHolder.of(context).card,
+          elevation: 2,
+          shadowColor: ThemeHolder.of(context).cardShadow,
+          title: Text(
+            "Параметры",
+            style: ThemeHolder.of(context).textStyle.h2(color: textColor),
+          ),
+          leading: const _AppLogo(),
         );
       case AppTab.settings:
-        return const SettingsScreen();
+        return AppBar(
+          centerTitle: true,
+          backgroundColor: ThemeHolder.of(context).card,
+          elevation: 2,
+          shadowColor: ThemeHolder.of(context).cardShadow,
+          title: Text(
+            "Настройки",
+            style: ThemeHolder.of(context).textStyle.h2(color: textColor),
+          ),
+          leading: const _AppLogo(),
+        );
     }
   }
 }
+
+class _AppLogo extends StatelessWidget {
+  const _AppLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16),
+      child: AppIcon(
+        icon: AppIcons.logo,
+        color: ThemeHolder.of(context).warning,
+        height: 50,
+      ),
+    );
+  }
+}
+
